5799d58df8e1e9ba1fc662372841ab39b322018c
Daniel Sloof <daniel@rubic.nl>
2025-01-10 13:44:42 +0100

    fix test assuming 3 results instead of 2

--- store_test.go
+++ store_test.go
@@ -21,16 +21,13 @@ func TestDiscoverStores(t *testing.T) {
 	os.Setenv("HOME", fixtureBase+"/discovery")
 	stores := DiscoverStores()
 
-	assert.Len(t, stores, 3)
+	assert.Len(t, stores, 2)
 
 	assert.Equal(t, "WooCommerce", stores[0].Platform.Name())
 	assert.Contains(t, stores[0].DocRoot, "discovery/applications/123456789/public_html")
 
 	assert.Equal(t, "Magento 2", stores[1].Platform.Name())
 	assert.Contains(t, stores[1].DocRoot, "discovery/sansec.store")
-
-	assert.Equal(t, "Magento 2", stores[2].Platform.Name())
-	assert.Contains(t, stores[2].DocRoot, "discovery/sansec.store")
 }
 
 func TestDiscoverStoresEmpty(t *testing.T) {
