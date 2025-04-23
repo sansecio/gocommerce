9aec319141add9588ded54bf5baa1d478cfa9370
Daniel Sloof <daniel@rubic.nl>
2025-04-23 11:22:32 +0200

    support prestashop custom database port

---
+++ fixture/prestashop7/configs/custom-port.php
@@ -0,0 +1,24 @@
+<?php return array(
+  'parameters' =>
+  array(
+    'database_host' => '127.0.0.1',
+    'database_port' => '1234',
+    'database_name' => 'presta_db',
+    'database_user' => 'presta_user',
+    'database_password' => 'qwerty123',
+    'database_prefix' => 'myprefix_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'slkdjflksdflksdfjk',
+    'ps_caching' => 'CacheMemcached',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2011-07-01',
+    'locale' => 'en-US',
+    'cookie_key' => 'cookie-key-bogys',
+    'cookie_iv' => 'skldfksjdf',
+    'new_cookie_key' => 'sdjfksdfjkds',
+  ),
+);
--- prestashop7_config_test.go
+++ prestashop7_config_test.go
@@ -10,3 +10,8 @@ func TestPrestashop7Config(t *testing.T) {
 	dbc := dbConfigFromSource(t, fixtureBase+"/prestashop7/configs/normal.php", &Prestashop7{})
 	assert.Equal(t, "presta_user:qwerty123@tcp(127.0.0.1:3306)/presta_db?allowOldPasswords=true", dbc.DSN())
 }
+
+func TestPrestashop7ConfigWithCustomPort(t *testing.T) {
+	dbc := dbConfigFromSource(t, fixtureBase+"/prestashop7/configs/custom-port.php", &Prestashop7{})
+	assert.Equal(t, "presta_user:qwerty123@tcp(127.0.0.1:1234)/presta_db?allowOldPasswords=true", dbc.DSN())
+}
