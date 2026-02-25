7c0003a4360cf5886f655c5a3a60bbee242252bb
Daniel Sloof <daniel@rubic.nl>
2026-02-25 14:38:50 +0100

    deduplicate

--- prestashop7.go
+++ prestashop7.go
@@ -2,7 +2,6 @@ package gocommerce
 
 import (
 	"context"
-	"errors"
 	"path/filepath"
 	"strconv"
 
@@ -43,35 +42,7 @@ func (p *Prestashop7) BaseURLs(ctx context.Context, docroot string) ([]string, e
 	if err != nil {
 		return nil, err
 	}
-
-	db, err := ConnectDB(ctx, *cfg.DB)
-	if err != nil {
-		return nil, err
-	}
-
-	prefix, err := cfg.DB.SafePrefix()
-	if err != nil {
-		return nil, err
-	}
-
-	rows, err := db.Query(`SELECT DISTINCT domain_ssl FROM ` + prefix + `shop_url`)
-	if err != nil {
-		return nil, err
-	}
-
-	urls := []string{}
-	for rows.Next() {
-		var url string
-		if err := rows.Scan(&url); err == nil {
-			urls = append(urls, "https://"+url+"/")
-		}
-	}
-
-	if len(urls) > 0 {
-		return urls, nil
-	}
-
-	return nil, errors.New("base url(s) not found in database")
+	return prestashopBaseURLs(ctx, cfg)
 }
 
 func (p *Prestashop7) Version(docroot string) (string, error) {
@@ -79,22 +50,5 @@ func (p *Prestashop7) Version(docroot string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	db, err := ConnectDB(context.Background(), *cfg.DB)
-	if err != nil {
-		return "", err
-	}
-
-	prefix, err := cfg.DB.SafePrefix()
-	if err != nil {
-		return "", err
-	}
-
-	var version string
-	err = db.QueryRow(`SELECT value FROM ` + prefix + `configuration WHERE name = 'PS_VERSION_DB'`).Scan(&version)
-	if err != nil {
-		return "", err
-	}
-
-	return version, nil
+	return prestashopVersion(cfg)
 }
