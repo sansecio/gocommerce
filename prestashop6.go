ce84b4798eab63d8c1feaa8516e33e58eb28f39c
Daniel Sloof <daniel@rubic.nl>
2026-02-25 14:28:53 +0100

    add prestashop version/urls

--- prestashop6.go
+++ prestashop6.go
@@ -1,7 +1,10 @@
 package gocommerce
 
 import (
+	"context"
+	"errors"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -56,4 +59,65 @@ func (p *Prestashop6) ParseConfig(cfgPath string) (*StoreConfig, error) {
 			Port:   port,
 		},
 	}, nil
+}
+
+func (p *Prestashop6) BaseURLs(ctx context.Context, docroot string) ([]string, error) {
+	cfg, err := p.ParseConfig(filepath.Join(docroot, p.ConfigPath()))
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := ConnectDB(ctx, *cfg.DB)
+	if err != nil {
+		return nil, err
+	}
+
+	prefix, err := cfg.DB.SafePrefix()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := db.Query(`SELECT DISTINCT domain_ssl FROM ` + prefix + `shop_url`)
+	if err != nil {
+		return nil, err
+	}
+
+	urls := []string{}
+	for rows.Next() {
+		var url string
+		if err := rows.Scan(&url); err == nil {
+			urls = append(urls, "https://"+url+"/")
+		}
+	}
+
+	if len(urls) > 0 {
+		return urls, nil
+	}
+
+	return nil, errors.New("base url(s) not found in database")
+}
+
+func (p *Prestashop6) Version(docroot string) (string, error) {
+	cfg, err := p.ParseConfig(filepath.Join(docroot, p.ConfigPath()))
+	if err != nil {
+		return "", err
+	}
+
+	db, err := ConnectDB(context.Background(), *cfg.DB)
+	if err != nil {
+		return "", err
+	}
+
+	prefix, err := cfg.DB.SafePrefix()
+	if err != nil {
+		return "", err
+	}
+
+	var version string
+	err = db.QueryRow(`SELECT value FROM ` + prefix + `configuration WHERE name = 'PS_VERSION_DB'`).Scan(&version)
+	if err != nil {
+		return "", err
+	}
+
+	return version, nil
 }
